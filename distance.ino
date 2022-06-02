57b6d938f131761192af8f6cc54ef1c116d12331
andradagobeaja <95458754+andradagobeaja@users.noreply.github.com>
2022-06-02 11:25:17 +0300

    Update distance.ino

--- distance.ino
+++ distance.ino
@@ -1,7 +1,7 @@
 void distanceSetup()
 {
-  pinMode(trig, OUTPUT); // Set the trigger pin as OUTPUT
-  pinMode(echo, INPUT); // Set the echo pin as INPUT
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
 
 }
 
@@ -9,11 +9,11 @@ void distanceFcn()
 {
   digitalWrite(trig, LOW);
   delayMicroseconds(5);
-  digitalWrite(trig, HIGH); // Set the trigger pin HIGH to send the ultrasonic wave (pulse)
+  digitalWrite(trig, HIGH); 
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
 
-  duration = pulseIn(echo, HIGH); // Calculate time taken (in microseconds) for the pulse emitted by the trigger pin to reach the echo pin.
-  distance = (duration / 2) * (331.3 / 10000); // Calculate the distance from the sensor to the obstacle in cm, using the speed of sound in air(m/s) and the time taken (stored in duration variable)
+  duration = pulseIn(echo, HIGH); 
+  distance = (duration / 2) * (331.3 / 10000); 
   Serial.println(distance);
 }
